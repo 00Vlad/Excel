2b416434ab1bc9abe64db9c85074b99adfd88c9b
Vlad <57154809+Vlad043@users.noreply.github.com>
2020-12-17 13:53:18 +0100

    DOM class

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,14 +1,46 @@
 class Dom {
-  //constructor() {}
+  constructor(selector) {
+    this.$el =
+      typeof selector === "string"
+        ? document.querySelector(selector)
+        : selector
+  }
+
+  html(html) {
+    if (typeof html === "string") {
+      this.$el.innerHTML = html
+      return this
+      // для успешного выполнения чейна (метод должен что-то вернуть)
+    }
+    return this.$el.outerHTML.trim()
+  }
+
+  clear() {
+    this.html("")
+    return this
+  }
+
+  // полифилл для метода append
+  append(node) {
+    if (node instanceof Dom)
+      node == node.$el
+
+    if (Element.prototype.append)
+      this.$el.append(node.$el)
+    else
+      this.$el.appendChild(node.$el)
+
+    return this
+  }
 }
 
-export function $() {
-  return new Dom()
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = "") => {
   const el = document.createElement(tagName)
   if (classes) el.classList.add(classes)
 
-  return el
+  return $(el)
 }
