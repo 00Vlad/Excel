334db3ae36359ed303bf5c1a71fc1886087b1600
Vlad <57154809+Vlad043@users.noreply.github.com>
2020-12-26 21:28:48 +0100

    Rows and colums resizing

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from "@core/ExcelComponent"
 import { createTable } from "./table.template"
+import {$} from '@core/dom'
 
 export class Table extends ExcelComponent {
   static className = "excel__table"
@@ -7,11 +8,58 @@ export class Table extends ExcelComponent {
   constructor($root) {
     super($root, {
       name: 'Table',
-      //listeners: ['click', 'mousedown', 'mouseup', 'mousemove']
+      listeners: ['mousedown', 'mouseup']
     })
   }
 
   toHTML() {
     return createTable(25)
   }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest("[data-type='resizable']")
+      const coords = $parent.getCoords()
+      const type = $resizer.data.resize
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
+      document.onmousemove = e => {
+        if (type === 'col') {
+          const delta = e.pageX - coords.right
+          const value = coords.width + delta
+          $parent.$el.style.width = value + 'px'
+          cells.forEach(item => item.style.width = value + 'px')
+        } else {
+          const delta = e.pageY - coords.bottom
+          const value = coords.height + delta
+          $parent.$el.style.height = value + 'px'
+        }
+      }
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
+    }
+  }
+
+  onMouseup() {}
 }
+
+
+//const resizeType = event.target.dataset.resize
+//if (resizeType === 'col') {
+//  const delta = e.pageX - coords.right
+//  const value = coords.width + delta
+//  $parent.$el.style.width = value + 'px'
+
+//  const colIndex = $parent.$el.dataset.col
+//  const row = document.querySelectorAll(`[data-col='${colIndex}']`)
+
+//  row.forEach(item => {
+//    item.style.width = value + 'px'
+//  })
+//} else {
+//  const delta = e.pageY - coords.top
+//  $parent.$el.style.height = delta + 'px'
+//}
